c5c128a7a68f50c39ceecfa7c599bfde6f61ec65
Elena <marenina.e@yandex.com>
2023-03-01 14:34:55 +0300

    Решение выполнено

--- Task/Program.cs
+++ Task/Program.cs
@@ -8,3 +8,52 @@
 // ["1234", "1567", "-2", "computer science"] -> ["-2"]
 // ["Russia", "Denmark", "Kazan"] -> []
 
+string [] firstArray = CreateArrayString();
+PrintArray(firstArray);
+Console.Write(" -> ");
+int count = CountElements(firstArray);
+string[] secondArray = SecondArray(count, firstArray);
+PrintArray(secondArray);
+
+
+string [] CreateArrayString()
+{
+    string[] arr = new string[] { "Sunday", "1", "day", "Wednesday", "Th", "Friday", "Sat" };
+    return arr;
+}
+
+void PrintArray(string[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write(arr[i] + ", ");
+        else Console.Write(arr[i]);
+    }
+    Console.Write("]");
+}
+
+int CountElements(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3) count++;
+    }
+    return count;
+}
+
+string[] SecondArray(int size, string[] arr)
+{
+    string[] secArr = new string[size];
+    int j = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        { 
+            secArr[j] = arr[i];
+            j++;
+        }
+    }
+    return secArr;
+}
